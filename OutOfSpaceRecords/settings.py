d6c6bb2a8e45c1cb31d8378ac0de09bd0e76989d
crabtree0 <106970253+crabtree0@users.noreply.github.com>
2022-10-16 19:42:01 +0100

    Update settings.py

--- OutOfSpaceRecords/settings.py
+++ OutOfSpaceRecords/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-^gm6!vkbcv%54efi+q@ou-mg6ro9-wa&++@*xt$qwtfnp111jg'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
